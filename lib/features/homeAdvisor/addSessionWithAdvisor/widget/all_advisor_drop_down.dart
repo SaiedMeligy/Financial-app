7afb9b93e9d08aa000f5456695ba9029e755b924
islami app <saidmeligy171@gmail.com>
2024-10-04 14:21:07 +0300

    f

--- lib/features/homeAdvisor/addSessionWithAdvisor/widget/all_advisor_drop_down.dart
+++ lib/features/homeAdvisor/addSessionWithAdvisor/widget/all_advisor_drop_down.dart
@@ -8,7 +8,7 @@ import 'package:experts_app/features/homeAdmin/addSession/widget/manager/states.
 class DropdownButtonAdvisor extends StatefulWidget {
   final Function(String) onAdvisorSelected;
 
-  DropdownButtonAdvisor({required this.onAdvisorSelected});
+  const DropdownButtonAdvisor({required this.onAdvisorSelected});
 
   @override
   _DropdownButtonAdvisorState createState() => _DropdownButtonAdvisorState();
@@ -32,56 +32,55 @@ class _DropdownButtonAdvisorState extends State<DropdownButtonAdvisor> {
       bloc: allAdvisorsCubit,
       builder: (context, state) {
         if (state is LoadingAllAdvisorState) {
-          return CircularProgressIndicator();
+          return Center(child: CircularProgressIndicator());
         } else if (state is SuccessAllAdvisorState) {
           var advisors = state.advisors;
           return Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1,
-                )
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.grey, width: 1),
             ),
-            child: DropdownButton<String>(
-              dropdownColor: Constants.theme.primaryColor.withOpacity(0.8),
-              value: selectedValue,
-              hint: Text('اختر الاستشاري',
-                style: Constants.theme.textTheme.bodyLarge?.copyWith(
-                  color: Colors.black
-                ),
-              ),
-              icon: Icon(Icons.arrow_downward, color: Colors.black87),
-              iconSize: 24,
-              elevation: 16,
-              style: TextStyle(color: Colors.black),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedValue = newValue;
-                  // selectedAdvisorId = advisors.firstWhere((advisor) => advisor.name == newValue).id.toString();
-                  selectedAdvisorId = newValue;
-                });
-                widget.onAdvisorSelected(selectedAdvisorId!);
-              },
-              items: advisors.map<DropdownMenuItem<String>>((advisor) {
-                return DropdownMenuItem<String>(
-                  value: advisor.id.toString(),
-                  child: Text(
-                    advisor.name.toString(),
-                    textAlign: TextAlign.center,
-                    style: Constants.theme.textTheme.bodyLarge
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                dropdownColor: Constants.theme.primaryColor.withOpacity(0.8),
+                value: selectedValue,
+                hint: Text(
+                  'اختر الاستشاري',
+                  style: Constants.theme.textTheme.bodyLarge?.copyWith(
+                    color: Colors.black,
                   ),
-                );
-              }).toList(),
+                ),
+                icon: Icon(Icons.arrow_downward, color: Colors.black87),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.black),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedValue = newValue;
+                    selectedAdvisorId = newValue;
+                  });
+                  widget.onAdvisorSelected(selectedAdvisorId!);
+                },
+                items: advisors.map<DropdownMenuItem<String>>((advisor) {
+                  return DropdownMenuItem<String>(
+                    value: advisor.id.toString(),
+                    child: Text(
+                      advisor.name.toString(),
+                      textAlign: TextAlign.center,
+                      style: Constants.theme.textTheme.bodyLarge,
+                    ),
+                  );
+                }).toList(),
+              ),
             ),
           );
         } else if (state is ErrorAllAdvisorState) {
-          return Text(state.errorMessage);
+          return Center(child: Text(state.errorMessage));
         } else {
-          return Text('Error');
+          return Center(child: Text('Error loading advisors'));
         }
       },
     );
   }
 }
-
